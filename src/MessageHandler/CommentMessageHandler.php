5af25e197eff430aa03569577be7ab634c5f7dfd
Salah <radi.salah12@gmail.com>
2020-05-16 18:22:34 +0200

    send email to admin for validate comment

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -5,6 +5,8 @@ use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
@@ -18,8 +20,12 @@ class CommentMessageHandler implements MessageHandlerInterface
     private $bus;
     private $workflow;
     private $logger;
-
-    public function __construct(EntityManagerInterface $entityManager, SpamChecker $spamChecker, CommentRepository $commentRepository, MessageBusInterface $bus, WorkflowInterface $commentStateMachine, LoggerInterface $logger = null)
+    private $mailer;
+    private $adminEmail;
+    public function __construct(EntityManagerInterface $entityManager, SpamChecker $spamChecker,
+                                CommentRepository $commentRepository, MessageBusInterface $bus,
+                                WorkflowInterface $commentStateMachine, LoggerInterface $logger = null,
+                                MailerInterface $mailer, string $adminEmail)
     {
         $this->entityManager = $entityManager;
         $this->spamChecker = $spamChecker;
@@ -27,6 +33,8 @@ class CommentMessageHandler implements MessageHandlerInterface
         $this->bus = $bus;
         $this->workflow = $commentStateMachine;
         $this->logger = $logger;
+        $this->mailer = $mailer;
+        $this->adminEmail = $adminEmail;
     }
 
     public function __invoke(CommentMessage $message)
@@ -49,8 +57,15 @@ class CommentMessageHandler implements MessageHandlerInterface
             $this->bus->dispatch($message);
 
         } elseif ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
-            $this->workflow->apply($comment, $this->workflow->can($comment,'publish') ? 'publish' : 'publish_ham');
-            $this->entityManager->flush();
+            //$this->workflow->apply($comment, $this->workflow->can($comment,'publish') ? 'publish' : 'publish_ham');
+            //$this->entityManager->flush();
+            $this->mailer->send((new NotificationEmail())
+                ->subject('New comment posted')
+                ->htmlTemplate('emails/comment_notification.html.twig')
+                ->from($this->adminEmail)
+                ->to($this->adminEmail)
+                ->context(['comment' => $comment])
+            );
         } elseif ($this->logger) {
             $this->logger->debug('Dropping comment message', ['comment' =>$comment->getId(), 'state' => $comment->getState()]);
         }
