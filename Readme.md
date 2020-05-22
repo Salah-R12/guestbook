a07194015ebdf4abe759d8fba7a4dc1ee8a36a10
Salah <radi.salah12@gmail.com>
2020-05-22 17:47:46 +0200

    cron to delete rejected comment

--- Readme.md
+++ Readme.md
@@ -1,3 +1,8 @@
 Cache :
 url  -I -X PURGE -u admin:admin "https://127.0.0.1:8000/admin/http-cache/conference_header" --ssl-no
 curl  -I -X PURGE -u admin:admin "https://127.0.0.1:8000/admin/http-cache" --ssl-no-revoke
+
+Delete old comments<br/>
+``
+symfony console app:comment:cleanup
+``
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -5,8 +5,10 @@ namespace App\Repository;
 use App\Entity\Comment;
 use App\Entity\Conference;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
+use phpDocumentor\Reflection\Types\AbstractList;
 
 /**
  * @method Comment|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,7 +19,7 @@ use Doctrine\Persistence\ManagerRegistry;
 class CommentRepository extends ServiceEntityRepository
 {
     public const PAGINATOR_PER_PAGE = 2;
-
+    private  const DAYS_BEFORE_REJECTED_REMOVAL  = 1;
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comment::class);
@@ -36,6 +38,28 @@ class CommentRepository extends ServiceEntityRepository
             ->getQuery()
         ;
         return new Paginator($query);
+    }
+    public function countOldRejected(): int
+    {
+        return $this->getOldRejectedQueryBuilder()->select('COUNT(c.id)')->getQuery()->getSingleScalarResult();
+    }
+
+    public function deleteOldRejected(): int
+    {
+        return $this->getOldRejectedQueryBuilder()->delete()->getQuery()->execute();
+    }
+
+    private function getOldRejectedQueryBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.state = :state_rejected or c.state = :state_spam')
+            ->andWhere('c.createdAt < :date')
+            ->setParameters([
+                'state_rejected' => 'rejected',
+                'state_spam' => 'spam',
+                'date' => new \DateTime(-self::DAYS_BEFORE_REJECTED_REMOVAL.'days'),
+            ])
+            ;
     }
     // /**
     //  * @return Comment[] Returns an array of Comment objects
