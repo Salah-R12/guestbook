7e9d7e2db55c9191c8c095b9926251a8c765ed80
Salah <radi.salah12@gmail.com>
2020-05-30 14:34:06 +0200

    api platform

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -2,37 +2,52 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ConferenceRepository")
  * @UniqueEntity("slug")
+ *
+ * @ApiResource(
+ *     collectionOperations={"get"={"normalization_context"={"groups"="conference:list"}}},
+ *     itemOperations={"get"={"normalization_context"={"groups"="conference:item"}}},
+ *     order={"year"="DESC", "ville"="ASC"},
+ *     paginationEnabled=false
+ * )
  */
+
 class Conference
 {
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     *
+     * @Groups({"conference:list", "conference:item"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"conference:list", "conference:item"})
      */
     private $ville;
 
     /**
      * @ORM\Column(type="string", length=4)
+     * @Groups({"conference:list", "conference:item"})
      */
     private $year;
 
     /**
      * @ORM\Column(type="boolean")
+     * @Groups({"conference:list", "conference:item"})
      */
     private $IsInternational;
 
